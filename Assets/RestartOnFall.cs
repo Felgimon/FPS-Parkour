ccbed657098cc17bdb5291d664fe32bc5453eede
Felgi <felipeferraridoval@gmail.com>
2025-05-19 11:53:37 -0300

    Initial commit

---
+++ Assets/RestartOnFall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartOnFall : MonoBehaviour
+{
+    public float yBoundery;
+
+    void Update()
+    {
+        if (transform.position.y < yBoundery)
+        {
+            Debug.Log("Te caiste");
+            SceneManager.LoadScene("Parkour");
+        }
+    }
+}
