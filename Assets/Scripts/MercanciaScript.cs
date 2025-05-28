295048f42fb732942bb0f8465815eb7981f1d50c
Felgi <felipeferraridoval@gmail.com>
2025-05-28 15:49:42 -0300

    Destruccion de objetos y mejora de nivel

---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
